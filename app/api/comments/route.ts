cfeafa53988ed412db4cc6dea363bff21fef908c
Marek Bareła <marek.barela@wp.pl>
2024-04-07 12:50:24 +0200

    add user id to comment

--- app/api/comments/route.ts
+++ app/api/comments/route.ts
@@ -1,17 +1,23 @@
-import { NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
 
 const prisma = new PrismaClient();
 
-export async function POST(req: NextRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest) {
   const { content, postId } = await req.json();
+  const { userId } = auth();
 
   try {
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
     const comment = await prisma.comment.create({
       data: {
         content,
         postId: Number(postId),
+        userId,
       },
     });
 
